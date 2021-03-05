9c14ebfb99fd01c975fe670ade22938620b148f7
Jörg Hofmann <joerg@thuenic.de>
2021-03-05 12:05:26 +0100

    ScribanRenderer und Template hinzugefügt

--- csharp/IAD-MakeTimeTable/IAD-MakeTimeTable/Program.cs
+++ csharp/IAD-MakeTimeTable/IAD-MakeTimeTable/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text.Json;
 using Scriban;
+using Scriban.Runtime;
 
 namespace IAD_MakeTimeTable
 {
@@ -14,8 +15,7 @@ namespace IAD_MakeTimeTable
             IADPlan DerPlan = JsonSerializer.Deserialize<IADPlan>(contents);
             // Console.WriteLine(JsonSerializer.Serialize(DerPlan, new JsonSerializerOptions() { WriteIndented = true })); // Test-Ausgabe der Daten
             string templateFile = System.IO.File.ReadAllText("C:\\Users\\lokaleradmin\\Documents\\template.tmpl", System.Text.Encoding.UTF8);
-            var template = Template.Parse(templateFile);
-            var result = template.Render(DerPlan);
+            string result = ScribanRenderer.Render(templateFile, DerPlan);
             Console.Write(result);
         }
     }
---
+++ csharp/IAD-MakeTimeTable/IAD-MakeTimeTable/ScribanRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scriban;
+using Scriban.Runtime;
+using System.Text.Json;
+using System.Dynamic;
+
+namespace IAD_MakeTimeTable
+{
+    public static class ScribanRenderer
+    {
+        public static string Render(string templateBody, Object model)
+        {
+            var template = Template.Parse(templateBody);
+            var vars = new ScriptObject();
+            vars.Import(model, renamer: r => r.Name);
+            var context = new TemplateContext { MemberRenamer = r => r.Name, MemberFilter = null };
+            context.PushGlobal(vars);
+            return template.Render(context);
+        }
+    }
+}
